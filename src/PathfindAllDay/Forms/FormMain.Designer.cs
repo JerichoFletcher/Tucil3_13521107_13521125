7785cfb5c10901f104d601e592e9095615d15c4e
JerichoFletcher <jericho.russel17@gmail.com>
2023-04-08 16:02:56 +0700

    chore: organize project files

--- src/PathfindAllDay/Forms/FormMain.Designer.cs
+++ src/PathfindAllDay/Forms/FormMain.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace PathfindAllDay {
-    partial class Form1 {
+    partial class FormMain {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -23,10 +23,17 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FormMain";
+            this.Text = "Pathfind All Day";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
